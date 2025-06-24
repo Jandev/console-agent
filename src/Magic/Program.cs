0c0f5fe8cf30604ff953b3874f45f87d2582b669
Jan de Vries <jan@jan-v.nl>
2025-06-24 13:19:34 +0200

    Updated to initial IApp structure

--- src/Magic/Program.cs
+++ src/Magic/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        services.AddLogging(logging =>
+        {
+            logging.ClearProviders();
+            logging.AddConsole();
+        });
+        services.AddTransient<IApp, HelloWorld>();
+    })
+    .Build();
+
+var app = host.Services.GetRequiredService<IApp>();
+app.Run();
+
+await host.RunAsync();
+
+public interface IApp
+{
+    void Run();
+}
+
+internal class HelloWorld : IApp
+{
+    private readonly ILogger<HelloWorld> _logger;
+
+    public HelloWorld(ILogger<HelloWorld> logger)
+    {
+        _logger = logger;
+    }
+
+    public void Run()
+    {
+        _logger.LogInformation("Hello, World!");
+    }
+}
+
+
